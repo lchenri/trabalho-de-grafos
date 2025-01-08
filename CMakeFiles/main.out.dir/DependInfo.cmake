f0b9e55af7ae43e1e4c0b40ee7de44c901f26495
lchenri <lucasbonicardoso@outlook.com>
2025-01-08 19:45:43 -0300

    Linux build

---
+++ CMakeFiles/main.out.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lucas/Documentos/trabalho/trabalho-de-grafos/src/grafo_lista.cpp" "CMakeFiles/main.out.dir/src/grafo_lista.cpp.o" "gcc" "CMakeFiles/main.out.dir/src/grafo_lista.cpp.o.d"
+  "/home/lucas/Documentos/trabalho/trabalho-de-grafos/src/grafo_matriz.cpp" "CMakeFiles/main.out.dir/src/grafo_matriz.cpp.o" "gcc" "CMakeFiles/main.out.dir/src/grafo_matriz.cpp.o.d"
+  "/home/lucas/Documentos/trabalho/trabalho-de-grafos/src/lista_encadeada.cpp" "CMakeFiles/main.out.dir/src/lista_encadeada.cpp.o" "gcc" "CMakeFiles/main.out.dir/src/lista_encadeada.cpp.o.d"
+  "/home/lucas/Documentos/trabalho/trabalho-de-grafos/src/main.cpp" "CMakeFiles/main.out.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.out.dir/src/main.cpp.o.d"
+  "" "main.out" "gcc" "CMakeFiles/main.out.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
